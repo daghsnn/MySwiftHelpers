0f7571d8ac3edb507d0c861067079ca5f710cfb9
Hasan Dağ <64082503+daghsnn@users.noreply.github.com>
2023-06-06 14:24:37 +0300

    Update Extension+TextView+AnimatedLink.swift
    textfield datepicker eklendi

--- AllShortenedThings/Extension-Examples/Extension+TextView+AnimatedLink.swift
+++ AllShortenedThings/Extension-Examples/Extension+TextView+AnimatedLink.swift
@@ -19,5 +19,40 @@ extension UITextView {
         }
         self.tintColor = linkColor ?? .blue
         self.attributedText = attributedOriginalText
-    }
+    }  
 }
+extension UITextField {
+  func setInputViewDatePicker(target: Any,title: String, selector: Selector, pickerMode: UIDatePicker.Mode = .time, minimumDate: Date? = nil, maximumDate: Date? = nil) {
+        
+        // Create a UIDatePicker object and assign to inputView
+        let screenWidth = UIScreen.main.bounds.width
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))
+        datePicker.datePickerMode = pickerMode
+        let loc = Locale(identifier: "tr")
+        datePicker.locale = loc
+        if let minimum = minimumDate {
+            datePicker.minimumDate = minimum
+        }
+        
+        if let max = maximumDate {
+            datePicker.maximumDate = max
+        }
+        
+        if #available(iOS 13.4, *) {
+            datePicker.preferredDatePickerStyle = .wheels
+        } else {
+            // Fallback on earlier versions
+        }
+        self.inputView = datePicker
+        
+        // Create a toolbar and assign it to inputAccessoryView
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancel = UIBarButtonItem(title: "Kapat", style: .plain, target: nil, action: #selector(tapCancel))
+        let barButton = UIBarButtonItem(title: "Seç", style: .plain, target: target, action: selector)
+        let titleLabel = UIBarButtonItem(title: title, style: .plain, target: target, action: nil)
+                
+        toolBar.setItems([cancel, flexible, titleLabel, flexible, barButton], animated: false)
+        self.inputAccessoryView = toolBar
+    }
+}  
